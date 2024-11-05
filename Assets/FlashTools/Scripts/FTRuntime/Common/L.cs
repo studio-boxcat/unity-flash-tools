301ebefb36a227cb7e720517ff704503f40e6397
James Kim <eodnjs2998@gmail.com>
2024-11-05 22:01:08 +0900

    removed Conditional attribute from error log

--- Assets/FlashTools/Scripts/FTRuntime/Common/L.cs
+++ Assets/FlashTools/Scripts/FTRuntime/Common/L.cs
@@ -11,9 +11,7 @@ namespace FTRuntime
         public static void I(string message, Object context = null) => Debug.Log($"[FlashTools] {message}", context);
         [Conditional("DEBUG")]
         public static void W(string message) => Debug.LogWarning($"[FlashTools] {message}");
-        [Conditional("DEBUG")]
         public static void E(string message) => Debug.LogError($"[FlashTools] {message}");
-        [Conditional("DEBUG")]
         public static void E(Exception exception) => Debug.LogError(exception);
     }
 }
